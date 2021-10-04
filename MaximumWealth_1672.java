02c55547f646b1dda59010004172a0b132942a52
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-04 12:17:50 +0530

    Update MaximumWealth_1672.java

--- MaximumWealth_1672.java
+++ MaximumWealth_1672.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<accounts.length;i++){
+            
+            int sum=0;
+            
+            for(int j=0;j<accounts[i].length;j++){
+                sum=sum+accounts[i][j];
+            }
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return max;
+    }
+}
