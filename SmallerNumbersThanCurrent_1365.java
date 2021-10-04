7a6eafab8433b783ef2bedb086b811a540a90f8c
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-04 12:59:19 +0530

    Update SmallerNumbersThanCurrent_1365.java

--- SmallerNumbersThanCurrent_1365.java
+++ SmallerNumbersThanCurrent_1365.java
@@ -1,1 +1,23 @@
 
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        
+        int []arr=new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    count++;
+                }
+                
+            }
+            arr[i]=count;
+            
+            
+        }
+        return arr;
+        
+    }
+}
