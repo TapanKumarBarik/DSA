826ffe2d4662f907d57b148e73e2e9fe22ed02fc
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-04 10:18:15 +0530

    Update FinalValueAfterOperations_2011.java

--- FinalValueAfterOperations_2011.java
+++ FinalValueAfterOperations_2011.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        
+        
+        int count=0;
+        for(int i=0;i<operations.length;i++){
+            
+            if(operations[i].charAt(1)=='+' ){
+                count++;
+            }
+              else {
+                count--;
+            }
+            
+        }
+        return count;
+        
+    }
+}
