20a3c90877ec53197b00ce1c8d69d8441e2893a4
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-04 12:38:57 +0530

    Update KidsWithCandies_1431.java

--- KidsWithCandies_1431.java
+++ KidsWithCandies_1431.java
@@ -1,1 +1,23 @@
 
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+     
+       List<Boolean> ans = new ArrayList<>(candies.length);
+        
+        int max=0;
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]>max){
+                max=candies[i];
+            }
+        }
+        
+        for(int j=0;j<candies.length;j++){
+          
+                ans.add(candies[j]+extraCandies>=max);
+           
+        }
+        
+        return ans;
+        
+    }
+}
