3798d507e300a8c2303a31b507ff0496076e3408
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-04 12:29:24 +0530

    Update ShuffleArray_1470.java

--- ShuffleArray_1470.java
+++ ShuffleArray_1470.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        
+        int[]arr=new int[n*2];
+        
+        for(int i=0;i<nums.length;i++){
+         if(i%2==0){
+             arr[i]=nums[i/2];
+             
+         }
+            else{
+                arr[i]=nums[n+i/2];
+            }
+            
+        }
+        return arr;
+    }
+}
