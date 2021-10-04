a859cea986283edafa1ddac6d8bc3a80818b4e26
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-04 12:46:02 +0530

    Update NumIdenticalPairs_1512.java

--- NumIdenticalPairs_1512.java
+++ NumIdenticalPairs_1512.java
@@ -1,1 +1,23 @@
 
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        
+        
+        int res=0;
+        
+        for(int i=0;i<nums.length-1;i++){
+            
+                 
+        for(int j=i+1;j<nums.length;j++){
+            
+            if(nums[i]==nums[j]){
+                res++;
+            }
+            
+            
+        }
+        }
+        return res;
+        
+    }
+}
