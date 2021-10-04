1fbc454c697cf4c3dc3e39f6731f8f246cc9d849
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-04 17:08:37 +0530

    Update CountKDifference_2006.java

--- CountKDifference_2006.java
+++ CountKDifference_2006.java
@@ -1,1 +1,16 @@
 
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        int res=0;
+        
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){   
+                if(nums[i] - nums[j] == k || nums[j]-nums[i]==k){
+                    res++;
+                }
+            }
+        }
+        return res;
+        
+    }
+}
