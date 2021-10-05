753083041c71c2fc821f9099e3f4fed91977661b
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-05 20:21:24 +0530

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,1 +1,20 @@
 
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int[] solve(final int[] A) {
+        
+        int b=A.length-1;
+        int a=0;
+        
+        while(a<b){
+           int temp=A[a];
+           A[a]=A[b];
+           A[b]=temp;
+            a++;
+            b--;
+            
+        }
+        return A;
+        
+    }
+}
