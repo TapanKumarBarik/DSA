7deaf407f8af57dc4032702a7279951ec429da83
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-05 11:03:21 +0530

    Create FindUniqueNumber.java

---
+++ FindUniqueNumber.java
@@ -0,0 +1,10 @@
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int singleNumber(final int[] A) {
+        int res=0;
+        for (int i=0;i<A.length;i++){
+            res=res ^A[i];
+        }
+        return res;
+    }
+}
