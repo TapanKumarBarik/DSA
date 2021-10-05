a7e5d6e13f48aba383f992f7239a995cdb775724
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-05 10:29:00 +0530

    Update AddBinaryString_67.java

--- AddBinaryString_67.java
+++ AddBinaryString_67.java
@@ -1,1 +1,36 @@
 
+class Solution {
+    public String addBinary(String a, String b) {
+     
+ int i = a.length() - 1;
+    int j = b.length() - 1;
+    int carry = 0;
+
+    StringBuilder sb = new StringBuilder();
+    while (i >= 0 || j >= 0) {
+
+      if (i >= 0) {
+        carry += a.charAt(i) - '0';
+
+      }
+      if (j >= 0) {
+        carry += b.charAt(j) - '0';
+      }
+      int temp = carry % 2;
+      carry = carry / 2;
+
+      sb.append(temp);
+
+      i--;
+      j--;
+    }
+
+    if (carry != 0) {
+      sb.append(carry);
+    }
+
+    return sb.reverse().toString();
+        
+        
+    }
+}
