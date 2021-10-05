7ae791c5c71f90908cf72a41b55d78067cd25dfe
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-05 08:05:52 +0530

    Update OddEvenDiff.java

--- OddEvenDiff.java
+++ OddEvenDiff.java
@@ -1,1 +1,22 @@
 
+public class Solution {
+    public int solve(int[] A) {
+        
+        int odd=Integer.MAX_VALUE;
+        int even=Integer.MIN_VALUE;
+        
+        for(int i=0;i<A.length;i++){
+            
+            if(A[i] % 2!=0 && A[i]<odd){
+                odd=A[i];
+                
+            }
+               if(A[i] % 2==0 && A[i]>even){
+                even=A[i];
+                
+            }
+        }
+        return even-odd;
+        
+    }
+}
