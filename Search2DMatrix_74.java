7f486281f7742bd5350d2b50115390d97b113968
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-08 16:26:44 +0530

    Update Search2DMatrix_74.java

--- Search2DMatrix_74.java
+++ Search2DMatrix_74.java
@@ -24,3 +24,42 @@ class Solution {
         
     }
 }
+
+
+
+//lon(n)
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int start=0;
+        int end=(matrix.length*matrix[0].length)-1;
+        
+        while(start<=end){
+            
+            int mid=start+(end-start)/2;
+            
+            int midElement=matrix[mid/matrix[0].length][mid%matrix[0].length];
+            
+           if(target==midElement){
+               return true;
+           }
+            
+            if(target>midElement){
+                
+                start=mid+1;
+            }
+            if(target<midElement){
+                end=mid-1;
+            }
+            
+            
+            
+            
+        }
+        
+        return false;
+        
+        
+    }
+}
