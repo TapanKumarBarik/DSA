715d983724737e34959257d846c454084e53850d
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-09 22:54:32 +0530

    Update MINIMUMPICKS.java

--- Learning/001/MINIMUMPICKS.java
+++ Learning/001/MINIMUMPICKS.java
@@ -1,1 +1,28 @@
 
+Problem Description
+
+You are given an array of integers A of size N.
+Return the difference between the maximum among all even numbers of A and the minimum among all odd numbers in A.
+
+
+
+Problem Constraints
+2 <= N <= 1e5
+-1e9 <= A[i] <= 1e9
+There is atleast 1 odd and 1 even number in A
+
+
+
+Input Format
+The first argument given is the integer array, A.
+
+
+
+Output Format
+Return maximum among all even numbers of A - minimum among all odd numbers in A.
+  
+  
+  
+  
+  
+  
