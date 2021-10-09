b5d44cec15f82e96429be8637b0b43d86a3ebdde
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-09 23:46:40 +0530

    Update SumOfN_WholeNumber.java

--- Learning/001/SumOfN_WholeNumber.java
+++ Learning/001/SumOfN_WholeNumber.java
@@ -1,1 +1,14 @@
 
+Sum of n natural whole number===== n(n-1)/2
+
+
+0,1,2,3,.....n-1 // as there are n numbers and start from 0 so last number is n-1;
+  
+  we know sum of n natural numbers =n(n+1)/2;
+
+so applying same here
+here n=n-1// last number
+so 
+
+(n-1)+(n-1+1)/2 
+  => n(n-1)/2 
