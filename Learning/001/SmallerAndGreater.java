589fa14a1f78be55da1eb1094f2109575c23757e
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-09 23:16:48 +0530

    Update SmallerAndGreater.java

--- Learning/001/SmallerAndGreater.java
+++ Learning/001/SmallerAndGreater.java
@@ -17,12 +17,21 @@ Constraints
 1 <= N <= 1e5
 1 <= A[i] <= 1e6
 For Example
-
+//1
 Example Input:
     A = [1, 2, 3]
 
 Example Output:
     1
 
+Explanation:
+    only 2 have a strictly smaller and strictly greater element, 1 and 3 respectively.
+ //2
+        
+ Example Input:
+    A = [1, 2, 3,3,3,3,3,3,3,3,3,3,3]
+
+Example Output:
+    1
 Explanation:
     only 2 have a strictly smaller and strictly greater element, 1 and 3 respectively.
