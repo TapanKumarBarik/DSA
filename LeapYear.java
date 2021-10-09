1ef627e0db4599c0e21bd7c1009f452e3d4d782d
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-09 14:38:24 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int solve(int A) {
+        
+        
+        int res=0;
+        
+        if(A%400==0 ||( A%4==0 && A%100!=0) ){
+            return 1;
+        }
+        return res;
+    }
+}
