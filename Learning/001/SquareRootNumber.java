a666b92a3df4ae6374d385e60e26b90ea5e0fe54
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-10 07:39:00 +0530

    Update SquareRootNumber.java

--- Learning/001/SquareRootNumber.java
+++ Learning/001/SquareRootNumber.java
@@ -1,1 +1,34 @@
 
+///
+
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+     
+     //binary search squareroot
+     
+     int num=49;
+     
+     int start=1;
+     int end=num;
+     
+     while(start<=num){
+       int mid=start+(end-start)/2;
+       if(mid*mid==num){
+         System.out.println(mid);
+         break;
+       }
+       if(mid*mid<num){
+         start=mid+1;
+       }
+         if(mid*mid>num){
+         end=mid-1;
+       }
+     }
+     
+     System.out.println("Number not found");
+    }
+}
+//////
+
