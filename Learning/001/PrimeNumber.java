62283f34276d0799ba806e7e3fe6d700a9f5c53b
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-10 07:46:43 +0530

    Update PrimeNumber.java

--- Learning/001/PrimeNumber.java
+++ Learning/001/PrimeNumber.java
@@ -1,1 +1,24 @@
 
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+     
+boolean isPrime=solve(4);
+System.out.println(isPrime);
+    }
+    static boolean solve(int num){
+       if(num==1){
+      return false;
+    }
+  for(int i=2;i*i<=num;i++){
+    
+   if(num%i==0){
+     return false;
+   }
+   
+  }
+      return true;
+      
+    }
+}
