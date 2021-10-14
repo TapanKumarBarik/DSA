377c61ed3bcf627105986fac79b6b7432dddd1a8
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-14 08:34:01 +0530

    Create SingleNumber.java

---
+++ Learning/IB/Bit Manipulation/SingleNumber.java
@@ -0,0 +1,11 @@
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int singleNumber(final int[] A) {
+
+        int result=0;
+        for(int i=0;i<A.length;i++){
+            result=result^A[i];
+        }
+        return result;
+    }
+}
