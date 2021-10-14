bfaff1fdf96d4b97c0bc54e97d0c68a3bb94d921
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-14 17:33:20 +0530

    Create XOR-ing the Subarrays!.java

---
+++ Learning/IB/Bit Manipulation/XOR-ing the Subarrays!.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int solve(int[] A) {
+        if(A.length%2==0) return 0;
+int res=0;
+for(int i=0;i<A.length;i=i+2){
+res=res^A[i];
+}
+
+ return res;   
+    }
+}
