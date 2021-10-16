0bedfafa2ad0b78b32445057f3f3bb12194f8fba
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-16 09:42:39 +0530

    Create Time Complexity - 1.0.java

---
+++ Learning/008/Time Complexity - 1.0.java
@@ -0,0 +1,11 @@
+void solve(){
+   for(int i = n; i > 0; i/=2){
+       if(i%2 == 0){
+           for(int j = 1; j <= n * n; j+=2){
+               // O(1) operation
+           }
+       }
+   }  
+}
+
+Time-O(N^2LogN)
