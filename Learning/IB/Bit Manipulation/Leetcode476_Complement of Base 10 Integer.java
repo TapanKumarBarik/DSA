8ce984213b0c1f9943ac0eb6e7426d66227bd467
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-16 20:08:50 +0530

    Create Leetcode476_Complement of Base 10 Integer.java

---
+++ Learning/IB/Bit Manipulation/Leetcode476_Complement of Base 10 Integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        
+        
+        int ans=0;
+        
+        int power=1;
+        
+        if(n==0){
+            return 1;
+        }
+        
+        while(n>0){
+            
+            int lastBit=n&1;
+            if(lastBit==0){
+                ans+=(1*power);
+            }
+            
+            n=n>>1;
+            power=power*2;
+        }
+        
+        
+        return ans;
+    }
+}
