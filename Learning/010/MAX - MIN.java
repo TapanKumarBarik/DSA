dee0e6b9ca5cf86308123d97396baaa089519096
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-18 12:17:08 +0530

    Create MAX - MIN.java

---
+++ Learning/010/MAX - MIN.java
@@ -0,0 +1,41 @@
+
+Given an array of integers A and an integer B, find and return the difference of B'th max element and B'th min element of the array A.
+
+
+Input Format
+
+The first argument given is the integer array A.
+The second argument given is integer B.
+Output Format
+
+Return the value of B'th max element of A - B'th min element of A.
+Constraints
+
+1 <= B <= length of the array <= 100000
+-10^9 <= A[i] <= 10^9 
+For Example
+
+Input 1:
+    A = [1, 2, 3, 4, 5]
+    B = 2
+Output 1:
+    2   (4 - 2 = 2)
+
+Input 2:
+    A = [5, 17, 100, 11]
+    B = 1
+Output 2:
+    95  (100 - 5 = 95)
+      
+      
+      
+      //
+      
+      public class Solution {
+    public int solve(int[] A, int B) {
+        
+        
+        Arrays.sort(A);
+        return A[A.length-B]-A[B-1];
+    }
+}
