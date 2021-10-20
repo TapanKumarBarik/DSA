33ee2694a851a67649af98a275e15f7d790d6e5c
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-20 12:16:00 +0530

    Update Closest MinMax.java

--- Learning/012/Closest MinMax.java
+++ Learning/012/Closest MinMax.java
@@ -103,3 +103,35 @@ Explanation 2:
 }
  
    
+*********************************************************
+Optimal
+***************************************************************************
+public class Solution {
+    public int solve(int[] A) {
+        
+        int min_Index = -1, max_Index = -1;
+        int min_ele = Integer.MAX_VALUE, max_ele = Integer.MIN_VALUE;
+        
+        int ans = Integer.MAX_VALUE;
+        for(int x:A)
+        {
+            min_ele = Math.min(min_ele, x);
+            max_ele = Math.max(max_ele, x);
+        }
+        
+        for(int i=0 ; i<A.length ; i++)
+        {
+            if(A[i] == min_ele) min_Index = Math.max(min_Index, i);
+            if(A[i] == max_ele) max_Index = Math.max(max_Index, i);
+            
+            if(min_Index != -1 && max_Index != -1)
+            {
+                int len = Math.abs(max_Index - min_Index) + 1;
+                ans = Math.min(ans, len);
+            }
+                
+        }
+        
+        return ans;
+    }
+}
