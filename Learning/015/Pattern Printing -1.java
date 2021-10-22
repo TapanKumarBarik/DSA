82eb5c94c96b768ad12d47acd7f17fc3a2d9e86d
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-22 23:52:53 +0530

    Create Pattern Printing -1.java

---
+++ Learning/015/Pattern Printing -1.java
@@ -0,0 +1,90 @@
+Problem Description
+
+Print a Pattern According to The Given Value of A.
+
+Example: For A = 3 pattern looks like:
+
+1 0 0
+
+1 2 0
+
+1 2 3
+
+
+
+Problem Constraints
+1 <= A <= 1000
+
+
+Input Format
+First and only argument is an integer denoting A.
+
+
+
+Output Format
+Return a two-dimensional array where each row in the returned array represents a row in the pattern.
+
+
+
+Example Input
+Input 1:
+
+ A = 3
+Input 2:
+
+ A = 4
+
+
+Example Output
+Output :1
+
+ [ 
+   [1, 0, 0]
+   [1, 2, 0]
+   [1, 2, 3]
+ ]
+Output 2:
+
+ [ 
+   [1, 0, 0, 0]
+   [1, 2, 0, 0]
+   [1, 2, 3, 0]
+   [1, 2, 3, 4]
+ ]
+
+
+Example Explanation
+Explanation 2:
+
+ 
+ For A = 4 pattern looks like:  
+                             1 0 0 0
+                             1 2 0 0
+                             1 2 3 0
+                             1 2 3 4
+ So we will return it as two-dimensional array.
+
+
+                               
+                               
+                               
+                               
+                               
+                               
+                               
+                               
+                               
+  public class Solution {
+    public int[][] solve(int A) {
+        
+        int[][]arr=new int[A][A];
+        for(int i=0;i<A;i++){
+            int count=1;
+            for(int j=0;j<=i;j++){
+                arr[i][j]=count;
+                count++;
+            }
+        }
+        return arr;
+    }
+}
