96f493b3e62af254e660ec188928d0de88f4e3d5
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-23 00:17:54 +0530

    Create Rotate Matrix.java

---
+++ Learning/015/Rotate Matrix.java
@@ -0,0 +1,88 @@
+Problem Description
+
+You are given a n x n 2D matrix A representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+You need to do this in place.
+
+Note: If you end up using an additional array, you will only receive partial score.
+
+
+
+Problem Constraints
+1 <= n <= 1000
+
+
+
+Input Format
+First argument is a 2D matrix A of integers
+
+
+
+Output Format
+Return the 2D rotated matrix.
+
+
+
+Example Input
+Input 1:
+
+ [
+    [1, 2],
+    [3, 4]
+ ]
+Input 2:
+
+ [
+    [1]
+ ]
+
+
+Example Output
+Output 1:
+
+ [
+    [3, 1],
+    [4, 2]
+ ]
+Output 2:
+
+ [
+    [1]
+ ]
+
+
+Example Explanation
+Explanation 1:
+
+ After rotating the matrix by 90 degree:
+ 1 goes to 2, 2 goes to 4
+ 4 goes to 3, 3 goes to 1
+Explanation 2:
+
+ 2D array remains the ssame as there is only element.
+
+
+
+public class Solution {
+    public void solve(int[][] matrix) {
+        
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i; j<matrix[0].length; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i =0 ; i<matrix.length; i++){
+            for(int j = 0; j<matrix.length/2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+}
