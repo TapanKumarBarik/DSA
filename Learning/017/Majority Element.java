6a8c445f14aeb3081f036ff732d11ccae827c9ad
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-10-25 08:35:14 +0530

    Update Majority Element.java

--- Learning/017/Majority Element.java
+++ Learning/017/Majority Element.java
@@ -17,3 +17,31 @@ Return  : 2 which occurs 2 times which is greater than 3/2.
         return A[A.length/2];
     }
 }
+
+
+
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int majorityElement(final int[] A) {
+        
+       int []arr= new int[1];
+       int count=0;
+       
+       
+       for(int i=0;i<A.length;i++){
+           
+           
+           if(A[i]==arr[0]){
+               count++;
+           }
+           else if(A[i]!=arr[0] && count==0){
+               arr[0]=A[i];
+               count++;
+           }
+           else{
+               count--;
+           }
+       }
+       return arr[0];
+    }
+}
