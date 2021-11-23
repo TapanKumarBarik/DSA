1828bb7b5aafa7720821e25fd8565dcc99212825
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-11-23 06:11:46 +0530

    Create HLD - Denormalization 2.java

---
+++ Learning/Advance Test Practice/HLD - Denormalization 2.java
@@ -0,0 +1,12 @@
+Which of the following statements is FALSE with respect to Denormalization?
+  
+  
+  
+      A denormalized database may perform worse than its normalized counterpart in case of heavy write load
+    Denormalized database improves read and write performance when compared against its normalized counterpart
+    Denormalization results in duplicated data
+    In normalized databases, a read resulting in a complex database join can turn out to be very expensive
+    
+    
+    
+Expected Answer: Denormalized database improves read and write performance when compared against its normalized counterpart
