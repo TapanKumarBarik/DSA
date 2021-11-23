e1af56c1ae39323cbedc951a63ce7078259008d4
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-11-23 06:09:39 +0530

    Create HLD - CDNs 1.java

---
+++ Learning/Advance Test Practice/HLD - CDNs 1.java
@@ -0,0 +1,11 @@
+CDNs are used for
+  
+  
+      Serving dynamic backend APIs
+    Increasing Read and Write throughput of your application
+    Serving static content like images, js files
+    Reducing write latency
+    
+    
+    
+    Expected Answer: Serving static content like images, js files
