14997974a4acb82cdbf41a25242becf0b15db5d6
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-11-25 12:05:44 +0530

    Update 41. First Missing Positive.java

--- DSA/Arrays/Hard/41. First Missing Positive.java
+++ DSA/Arrays/Hard/41. First Missing Positive.java
@@ -44,3 +44,48 @@ Constraints:
         return n+1;
     }
 }
+
+
+
+
+
+
+//Approach 2
+
+
+class Solution {
+    public int firstMissingPositive(int[] A) {
+      //   //Approach 1
+      //   int n=nums.length;
+      //   boolean []arr=new boolean [n]; 
+      //   for (int i = 0; i < n; i++) {
+      // if (nums[i] >= 1 && nums[i] <= n) {
+      //   arr[nums[i] - 1] = true;
+      // }   
+      //   } 
+      //   for(int i=0;i<n;i++){   
+      //       if(!arr[i]){
+      //           return i+1;
+      //       }
+      //   }
+      //   return n+1;
+        
+  int i = 0;
+        while(i < A.length){
+            if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
+            else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
+            else i++;
+        }
+        i = 0;
+        while(i < A.length && A[i] == i+1) i++;
+        return i+1;
+    }
+    
+    
+    
+    private void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
