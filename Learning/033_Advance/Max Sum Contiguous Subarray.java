cf2a5af884058d16eaccccb5960a485e57eeb1c9
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-11-27 09:40:23 +0530

    Create Max Sum Contiguous Subarray.java

---
+++ Learning/033_Advance/Max Sum Contiguous Subarray.java
@@ -0,0 +1,79 @@
+Problem Description
+
+Find the contiguous subarray within an array, A of length N which has the largest sum.
+
+
+
+Problem Constraints
+1 <= N <= 1e6
+-1000 <= A[i] <= 1000
+
+
+
+Input Format
+The first and the only argument contains an integer array, A.
+
+
+
+Output Format
+Return an integer representing the maximum possible sum of the contiguous subarray.
+
+
+
+Example Input
+Input 1:
+
+ A = [1, 2, 3, 4, -10] 
+Input 2:
+
+ A = [-2, 1, -3, 4, -1, 2, 1, -5, 4] 
+
+
+Example Output
+Output 1:
+
+ 10 
+Output 2:
+
+ 6 
+
+
+Example Explanation
+Explanation 1:
+
+ The subarray [1, 2, 3, 4] has the maximum possible sum of 10. 
+Explanation 2:
+
+ The subarray [4,-1,2,1] has the maximum possible sum of 6. 
+   
+   
+   
+   
+   public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int maxSubArray(final int[] A) {
+        
+        int start=0;
+        int n=A.length;
+        int end=n-1;
+        
+        int maxSum=Integer.MIN_VALUE;
+        int currentSum=0;
+        
+        while(start<=end){
+          
+            
+            currentSum+=A[start];
+            
+              if(currentSum>maxSum){
+                maxSum=currentSum;
+            }
+            if(currentSum<0){
+                currentSum=0;
+            }
+            
+            start++;
+        }
+        return maxSum;
+    }
+}
