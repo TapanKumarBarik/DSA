6fb9ce383ffe2121b68b981c7c167e5138fd8d1d
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-12-08 17:24:34 +0530

    Create Pair Sum divisible by M.java

---
+++ Learning/035_Advance/Pair Sum divisible by M.java
@@ -0,0 +1,72 @@
+Problem Description
+
+Given an array of integers A and an integer B, find and return the number of pairs in A whose sum is divisible by B.
+
+Since the answer may be large, return the answer modulo (109 + 7).
+
+
+
+Problem Constraints
+1 <= length of the array <= 100000
+1 <= A[i] <= 109
+1 <= B <= 106
+
+
+
+Input Format
+The first argument given is the integer array A.
+The second argument given is the integer B.
+
+
+
+Output Format
+Return the total number of pairs for which the sum is divisible by B modulo (109 + 7).
+
+
+
+Example Input
+Input 1:
+
+ A = [1, 2, 3, 4, 5]
+ B = 2
+Input 2:
+
+ A = [5, 17, 100, 11]
+ B = 28
+
+
+Example Output
+Output 1:
+
+ 4
+Output 2:
+
+ 1
+
+
+Example Explanation
+Explanation 1:
+
+ All pairs which are divisible by 2 are (1,3), (1,5), (2,4), (3,5). 
+ So total 4 pairs.
+
+   
+   
+   
+   
+   
+   
+   public class Solution {
+    public int solve(int[] a, int k) {
+        int n = a.length;
+        long mod = (long)(1e9 + 7);
+        long cnt[] = new long[k];
+        for(int x : a)    cnt[x % k]++;
+        long ans = cnt[0] * (cnt[0] - 1) / 2;
+        for(int i = 1, j = k-1; i <= j; i++, j--) {
+            if(i == j)    ans = (ans + cnt[i] * (cnt[i] - 1) / 2) % mod;
+            else    ans = (ans + cnt[i] * cnt[j]) % mod;
+        }
+        return (int)ans;
+    }
+}
