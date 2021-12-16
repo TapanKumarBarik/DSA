e5eb4c167cc992f524ea94b0230ad0ae5e59fc2f
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-12-16 10:02:50 +0530

    Update Sum of Digits!.java

--- Learning/041_Advance/Sum of Digits!.java
+++ Learning/041_Advance/Sum of Digits!.java
@@ -78,3 +78,19 @@ Explanation 2:
         
     }
 }
+
+
+
+
+
+
+
+//**************************************************************************************************8
+
+public class Solution {
+    public int solve(int A) {
+        if(A<10)return A;
+        return (A%10 +solve(A/10));
+    }
+}
+
