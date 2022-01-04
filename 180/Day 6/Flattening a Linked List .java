43d8192bbe65260bcfce8c8a9ac7174234513396
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-04 19:31:16 +0530

    Create Flattening a Linked List .java

---
+++ 180/Day 6/Flattening a Linked List .java
@@ -0,0 +1,233 @@
+Given a Linked List of size N, where every node represents a sub-linked-list and contains two pointers:
+(i) a next pointer to the next node,
+(ii) a bottom pointer to a linked list where this node is head.
+Each of the sub-linked-list is in sorted order.
+Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order. 
+Note: The flattened list will be printed using the bottom pointer instead of next pointer.
+
+ 
+
+Example 1:
+
+Input:
+5 -> 10 -> 19 -> 28
+|     |     |     | 
+7     20    22   35
+|           |     | 
+8          50    40
+|                 | 
+30               45
+Output:  5-> 7-> 8- > 10 -> 19-> 20->
+22-> 28-> 30-> 35-> 40-> 45-> 50.
+Explanation:
+The resultant linked lists has every 
+node in a single level.
+(Note: | represents the bottom pointer.)
+ 
+
+Example 2:
+
+Input:
+5 -> 10 -> 19 -> 28
+|          |                
+7          22   
+|          |                 
+8          50 
+|                           
+30              
+Output: 5->7->8->10->19->20->22->30->50
+Explanation:
+The resultant linked lists has every
+node in a single level.
+
+(Note: | represents the bottom pointer.)
+ 
+
+Your Task:
+You do not need to read input or print anything. Complete the function flatten() that takes the head of the linked list as input parameter and returns the head of flattened link list.
+
+ 
+
+Expected Time Complexity: O(N*M)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+0 <= N <= 50
+1 <= Mi <= 20
+1 <= Element of linked list <= 103
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // { Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node bottom;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+		bottom = null;
+	}
+}
+
+
+class Flatttening_A_LinkedList
+{	
+    Node head;
+	
+	void printList(Node node)
+    {
+        //Node temp = head;
+        while (node != null)
+        {
+            System.out.print(node.data + " ");
+            node =node.bottom;
+        }
+        System.out.println();
+    }
+	public  static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		Flatttening_A_LinkedList list = new Flatttening_A_LinkedList();
+		while(t>0)
+		{
+			int N = sc.nextInt();
+			int arr[] = new int[N];
+			for(int i=0;i<N;i++)
+				arr[i] = sc.nextInt();
+			
+			Node temp = null;
+			Node tempB = null;
+			Node pre = null;
+			Node preB = null;	
+			int flag=1;
+			int flag1=1;
+			for(int i=0; i<N;i++)
+			{
+				int m = arr[i];
+				m--;
+				int a1 = sc.nextInt();
+				temp = new Node(a1);
+				if(flag == 1)
+				{
+					list.head = temp;
+					pre = temp;
+					flag = 0;
+					flag1 = 1;
+				}
+				else
+				{
+					pre.next = temp;
+					pre = temp;
+					flag1 = 1;
+				}
+				
+				for(int j=0;j<m;j++)
+				{
+					int a = sc.nextInt();
+					tempB = new Node(a);
+					if(flag1 == 1)
+					{
+						temp.bottom = tempB;
+						preB = tempB;
+						flag1 = 0;
+					}
+					else
+					{
+						preB.bottom = tempB;
+						preB = tempB;
+					}
+				}
+				
+			}
+			//list.printList();
+			GfG g = new GfG();
+			Node root = g.flatten(list.head);
+			list.printList(root);
+		
+		t--;
+		}
+	}	
+}// } Driver Code Ends
+
+
+/*Node class  used in the program
+class Node
+{
+	int data;
+	Node next;
+	Node bottom;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+		bottom = null;
+	}
+}
+*/
+/*  Function which returns the  root of 
+    the flattened linked list. */
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+class GfG
+{
+    Node mergeTwoLists(Node a, Node b) {
+        
+        Node temp = new Node(0);
+        Node res = temp; 
+        
+        while(a != null && b != null) {
+            if(a.data < b.data) {
+                temp.bottom = a; 
+                temp = temp.bottom; 
+                a = a.bottom; 
+            }
+            else {
+                temp.bottom = b;
+                temp = temp.bottom; 
+                b = b.bottom; 
+            }
+        }
+        
+        if(a != null) temp.bottom = a; 
+        else temp.bottom = b;
+        return res.bottom; 
+    
+    }
+    Node flatten(Node root)
+    {
+       
+            if (root == null || root.next == null) 
+                return root; 
+      
+            // recur for list on right 
+            root.next = flatten(root.next); 
+      
+            // now merge 
+            root = mergeTwoLists(root, root.next); 
+      
+            // return the root 
+            // it will be in turn merged with its left 
+            return root; 
+    }
+}
+  
+  
