4cb2fe6b2fc8a22be41e251d1d47e0e5383c2683
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-05 18:58:03 +0530

    Update Matrix Search.java

--- Learning/046_Advance _BinarySearch1/Matrix Search.java
+++ Learning/046_Advance _BinarySearch1/Matrix Search.java
@@ -101,3 +101,27 @@ return 0;
 
 
 //Binary Search
+            
+            int low=0;
+            int high=A.length * (A[0].length)-1;
+            
+            while(low<=high){
+                
+                int mid=low+(high-low)/2;
+                int i=mid/A[0].length;
+                int j=mid%A[0].length;
+                
+                if(A[i][j]==B){
+                    return 1;
+                }
+                else if (A[i][j] >B){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+                
+                
+            }
+
+return 0;
