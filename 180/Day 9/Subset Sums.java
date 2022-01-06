d2af7b0464d84e59ee7619e67212a6f9f368e4ce
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-06 18:01:53 +0530

    Update Subset Sums.java

--- 180/Day 9/Subset Sums.java
+++ 180/Day 9/Subset Sums.java
@@ -95,3 +95,61 @@ class Solution{
 }
 
 //////////////////////////////////////////////////////////////////////////////
+
+
+
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			ArrayList<Integer> arr = new ArrayList<>();
+			for(int i = 0; i < N ; i++){
+			    arr.add(sc.nextInt());
+			}
+            Solution ob = new Solution();
+         
+            ArrayList<Integer> ans = ob.subsetSums(arr,N);
+            Collections.sort(ans);
+            for(int sum : ans){
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+        }  
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java//User function Template for Java
+class Solution{
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+        // code here
+   ArrayList<Integer>list=new ArrayList<Integer>();
+   int index=0;
+   int sum=0;
+   funct(index,sum,arr,N,list);
+   
+   Collections.sort(list);
+   return list;
+    }
+    
+    private void funct(int index, int sum, ArrayList<Integer> arr,int N,ArrayList<Integer> list){
+        if(index==N){
+            list.add(sum);
+            return;
+        }
+        funct(index+1,sum+arr.get(index),arr,N,list );
+        funct(index+1,sum,arr,N,list );
+        
+    }
+}
