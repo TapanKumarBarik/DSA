d0264a14e896fdb4142bc261e166af310f595a1a
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-06 17:20:10 +0530

    Create Subset Sums.java

---
+++ 180/Day 9/Subset Sums.java
@@ -0,0 +1,97 @@
+Given a list arr of N integers, print sums of all subsets in it.
+
+Note: Return all the element is increasing order.
+
+Example 1:
+
+Input:
+N = 2
+arr[] = {2, 3}
+Output:
+0 2 3 5
+Explanation:
+When no elements is taken then Sum = 0.
+When only 2 is taken then Sum = 2.
+When only 3 is taken then Sum = 3.
+When element 2 and 3 are taken then 
+Sum = 2+3 = 5.
+Example 2:
+
+Input:
+N = 3
+arr = {5, 2, 1}
+Output:
+0 1 2 3 5 6 7 8
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function subsetSums() which takes a list/vector and an integer N as an input parameter and return the list/vector of all the subset sums.
+
+Expected Time Complexity: O(2N)
+Expected Auxiliary Space: O(2N)
+
+Constraints:
+1 <= N <= 15
+0 <= arr[i] <= 104
+  
+  
+  
+  
+  
+  /////////////////////////////////////////////////////////////////////
+  
+  // { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			ArrayList<Integer> arr = new ArrayList<>();
+			for(int i = 0; i < N ; i++){
+			    arr.add(sc.nextInt());
+			}
+            Solution ob = new Solution();
+         
+            ArrayList<Integer> ans = ob.subsetSums(arr,N);
+            Collections.sort(ans);
+            for(int sum : ans){
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+        }  
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java//User function Template for Java
+class Solution{
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+        // code here
+      int powerSize= (int)Math.pow(2,arr.size());
+      ArrayList<Integer>list=new ArrayList<Integer>();
+        list.add(0);
+      for(int i=1;i<powerSize;i++){
+        int temp=i;//001
+        int sum=0;
+        int index=arr.size()-1;
+        while(temp>0){
+        if((temp & 1)==1){
+            sum+=arr.get(index);
+        }
+        index--;
+        temp=temp>>1;
+        }
+        list.add(sum);
+      }
+      return list;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
