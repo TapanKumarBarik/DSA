fcc52c37ee93639d2391a88979592df2d2c66170
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-11 20:25:02 +0530

    Create Pairs with given sum II.java

---
+++ Learning/049_Advance_TwoPointers1/Pairs with given sum II.java
@@ -0,0 +1,102 @@
+Problem Description
+
+Given a sorted array of integers (not necessarily distinct) A and an integer B, find and return how many pair of integers ( A[i], A[j] ) such that i != j have sum equal to B.
+
+Since the number of such pairs can be very large, return number of such pairs modulo (109 + 7).
+
+
+
+Problem Constraints
+1 <= |A| <= 100000
+
+1 <= A[i] <= 10^9
+
+1 <= B <= 10^9
+
+
+
+Input Format
+The first argument given is the integer array A.
+
+The second argument given is integer B.
+
+
+
+Output Format
+Return the number of pairs for which sum is equal to B modulo (10^9+7).
+
+
+
+Example Input
+Input 1:
+
+A = [1, 1, 1]
+B = 2
+Input 2:
+
+ 
+A = [1, 1]
+B = 2
+
+
+Example Output
+Output 1:
+
+ 3
+Output 2:
+
+ 1
+
+
+Example Explanation
+Explanation 1:
+
+ Any two pairs sum up to 2.
+Explanation 2:
+
+ only pair (1, 2) sums up to 2.
+   
+   
+   
+   
+   
+   
+   public class Solution {
+    public int solve(int[] A, int B) {
+        int i = 0;
+        int j = A.length - 1;
+        int mod = 1000 * 1000 * 1000 + 7;
+        long ans = 0;
+        while (i < j) {
+            if (A[i] + A[j] == B) {
+                int ii = i, jj = j;
+                if (A[i] == A[j]) {
+                    long cnt = j - i + 1;
+                    ans += (cnt * (cnt - 1) / 2) % mod;
+                    ans %= mod;
+                    break;
+                } else {
+                    // count number of elements with value A[i]
+                    while (A[i] == A[ii]) {
+                        ii++;
+                    }
+                    int cnt1 = ii - i;
+                    i = ii;
+
+                    // count number of elements with value A[i]
+                    while (A[jj] == A[j]) {
+                        jj--;
+                    }
+                    int cnt2 = j - jj;
+                    j = jj;
+                    ans += (cnt1 * cnt2) % mod;
+                    ans %= mod;
+                }
+            } else if (A[i] + A[j] > B) {
+                j--;
+            } else i++;
+        }
+        return (int) ans;
+    }
+}
+   
