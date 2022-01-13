6b9b64ea3bd1a9c7e8c52c233d13bfe6a99f891f
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-13 19:06:38 +0530

    Create Check Palindrome!.java

---
+++ Learning/050_Advanced_Hashing1/Check Palindrome!.java
@@ -0,0 +1,93 @@
+Problem Description
+
+Given a string A consisting of lowercase characters.
+
+Check if characters of the given string can be rearranged to form a palindrome.
+
+Return 1 if it is possible to rearrange the characters of the string A such that it becomes a palindrome else return 0.
+
+
+
+Problem Constraints
+1 <= |A| <= 105
+
+A consists only of lower-case characters.
+
+
+
+Input Format
+First argument is an string A.
+
+
+
+Output Format
+Return 1 if it is possible to rearrange the characters of the string A such that it becomes a palindrome else return 0.
+
+
+
+Example Input
+Input 1:
+
+ A = "abcde"
+Input 2:
+
+ A = "abbaee"
+
+
+Example Output
+Output 1:
+
+ 0
+Output 2:
+
+ 1
+
+
+Example Explanation
+Explanation 1:
+
+ No possible rearrangement to make the string palindrome.
+Explanation 2:
+
+ Given string "abbaee" can be rearranged to "aebbea" to form a palindrome.
+   
+   
+   
+   
+   
+   
+   
+   
+   public class Solution {
+    public int solve(String A) {
+        
+        HashMap<Character , Integer>map=new HashMap<Character,Integer>();
+        
+        for(int i=0;i<A.length();i++){
+            
+            if(map.containsKey(A.charAt(i))){
+                
+                int freq=map.get(A.charAt(i));
+                map.put(A.charAt(i),freq+1);
+            }
+            else{
+                map.put(A.charAt(i),1);
+            }
+        }
+        //
+       boolean oneOne=true;
+            for(int i=0;i<A.length();i++){
+              
+                 if(map.get(A.charAt(i))%2!=0 && oneOne==true){
+                    
+                    oneOne=false;
+                    int temp=map.get(A.charAt(i));
+                    map.put(A.charAt(i),temp-1);
+                }
+                  else if(map.get(A.charAt(i))%2!=0){
+                    return 0;
+                }
+            }
+            return 1;
+    }
+}
