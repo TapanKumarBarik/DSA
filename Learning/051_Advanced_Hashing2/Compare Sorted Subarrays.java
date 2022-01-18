53a46256bf5d0ccaf41407e9bc09152eadf740a5
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-18 19:08:14 +0530

    Update Compare Sorted Subarrays.java

--- Learning/051_Advanced_Hashing2/Compare Sorted Subarrays.java
+++ Learning/051_Advanced_Hashing2/Compare Sorted Subarrays.java
@@ -67,6 +67,49 @@ Explanation 2:
    
    
    //Optimal
+public class Solution {
+    public ArrayList<Integer> solve(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> B) {
+        
+        
+        int n=A.size();
+        Random rd=new Random();
+        HashMap<Integer,Long>hm=new HashMap();
+        HashSet<Long>used=new HashSet();
+        
+        long []psum=new long[n+1];
+        
+        for(int i=0;i<n;i++){
+            
+            if(!hm.containsKey(A.get(i))){
+                long hash=rd.nextLong();
+                while(used.contains(hash)){
+                    hash=rd.nextLong();
+                }
+                hm.put(A.get(i),hash);
+            }
+            psum[i+1]=psum[i]+hm.get(A.get(i));
+            
+        }
+        
+        
+        ArrayList<Integer>ans=new ArrayList();
+        
+        for(ArrayList<Integer>temp:B){
+            long val1=psum[temp.get(1)+1]-psum[temp.get(0)];
+             long val2=psum[temp.get(3)+1]-psum[temp.get(2)];
+             
+             
+             if(val1==val2){
+                 ans.add(1);
+             }
+             else{
+                 ans.add(0);
+             }
+        }
+        
+        return ans;
+    }
+}
 
 
 
