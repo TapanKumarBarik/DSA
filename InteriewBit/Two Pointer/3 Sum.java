63feaafc91321ba54ba279695c09abacb93616db
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-23 08:36:22 +0530

    Create 3 Sum.java

---
+++ InteriewBit/Two Pointer/3 Sum.java
@@ -0,0 +1,48 @@
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. 
+
+Return the sum of the three integers.
+
+Assume that there will only be one solution
+
+Example: 
+
+given array S = {-1 2 1 -4}, 
+
+and target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2)
+  
+  
+  
+  
+  public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int min = Integer.MAX_VALUE;
+	int result = 0;
+ 
+	Arrays.sort(nums);
+ 
+	for (int i = 0; i < nums.length; i++) {
+		int j = i + 1;
+		int k = nums.length - 1;
+		while (j < k) {
+			int sum = nums[i] + nums[j] + nums[k];
+			int diff = Math.abs(sum - target);
+ 
+			if(diff == 0) return sum;
+ 
+			if (diff < min) {
+				min = diff;
+				result = sum;
+			}
+			if (sum <= target) {
+				j++;
+			} else {
+				k--;
+			}
+		}
+	}
+ 
+	return result;
+    }
+}
