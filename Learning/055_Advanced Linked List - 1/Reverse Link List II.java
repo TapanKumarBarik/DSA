263885a7a69bf741543239fbc1d6816bc2967e8b
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-25 20:26:26 +0530

    Create Reverse Link List II.java

---
+++ Learning/055_Advanced Linked List - 1/Reverse Link List II.java
@@ -0,0 +1,123 @@
+Problem Description
+
+Reverse a linked list A from position B to C.
+
+NOTE: Do it in-place and in one-pass.
+
+
+
+Problem Constraints
+1 <= |A| <= 106
+
+1 <= B <= C <= |A|
+
+
+
+Input Format
+The first argument contains a pointer to the head of the given linked list, A.
+
+The second arugment contains an integer, B.
+
+The third argument contains an integer C.
+
+
+
+Output Format
+Return a pointer to the head of the modified linked list.
+
+
+
+Example Input
+Input 1:
+
+ A = 1 -> 2 -> 3 -> 4 -> 5
+ B = 2
+ C = 4
+
+Input 2:
+
+ A = 1 -> 2 -> 3 -> 4 -> 5
+ B = 1
+ C = 5
+
+
+Example Output
+Output 1:
+
+ 1 -> 4 -> 3 -> 2 -> 5
+Output 2:
+
+ 5 -> 4 -> 3 -> 2 -> 1
+
+
+Example Explanation
+Explanation 1:
+
+ In the first example, we want to reverse the highlighted part of the given linked list : 1 -> 2 -> 3 -> 4 -> 5 
+ Thus, the output is 1 -> 4 -> 3 -> 2 -> 5 
+Explanation 2:
+
+ In the second example, we want to reverse the highlighted part of the given linked list : 1 -> 4 -> 3 -> 2 -> 5  
+ Thus, the output is 5 -> 4 -> 3 -> 2 -> 1 
+   
+   
+   
+   
+   
+   
+   /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+
+  // Empty list
+        if (head == null) {
+            return null;
+        }
+
+        // Move the two pointers until they reach the proper starting point
+        // in the list.
+        ListNode cur = head;
+        ListNode prev = null;
+        while (m > 1) {
+            prev = cur;
+            cur = cur.next;
+            m--;
+            n--;
+        }
+
+        // The two pointers that will fix the final connections.
+        ListNode con = prev;
+        ListNode tail = cur;
+
+        // Iteratively reverse the nodes until n becomes 0.
+        ListNode third = null;
+        while (n > 0) {
+            third = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = third;
+            n--;
+        }
+
+        // Adjust the final connections as explained in the algorithm
+        if (con != null) {
+            con.next = prev;
+        } else {
+            head = prev;
+        }
+
+        tail.next = cur;
+        return head;
+    }
+
+        
+        
+   
+}
