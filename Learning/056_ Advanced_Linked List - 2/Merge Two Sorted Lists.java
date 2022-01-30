531c7ef1e61c3c11431bb4fb4c02c8ed5a3f1131
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-01-30 08:07:33 +0530

    Update Merge Two Sorted Lists.java

--- Learning/056_ Advanced_Linked List - 2/Merge Two Sorted Lists.java
+++ Learning/056_ Advanced_Linked List - 2/Merge Two Sorted Lists.java
@@ -122,4 +122,35 @@ public class Solution {
 
 
 
+
+
 //
+USING RECURSION
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode A, ListNode B) {
+        
+        if(A==null)return B;
+        if(B==null)return A;
+        
+     
+    if(A.val<B.val){
+        A.next=mergeTwoLists(A.next,B);
+        return A;
+    }
+    else{
+         B.next=mergeTwoLists(B.next,A);
+        return B;
+    }
+        
+    }
+}
+
