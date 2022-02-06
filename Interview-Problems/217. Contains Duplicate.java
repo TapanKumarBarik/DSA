6526b0aa280f10117bedd5084909e7a16b838c77
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-06 11:17:22 +0530

    Create 217. Contains Duplicate.java

---
+++ Interview-Problems/217. Contains Duplicate.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        
+        Arrays.sort(nums);
+        
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1])return true;
+        }
+        return false;
+    }
+}
+
+
+//or it can be solved using hashset
+
+
+
