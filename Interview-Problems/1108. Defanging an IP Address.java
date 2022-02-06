a9f0a0797f18fecec96af06cd5b9a28739f0cb79
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-06 11:15:57 +0530

    Update 1108. Defanging an IP Address.java

--- Interview-Problems/1108. Defanging an IP Address.java
+++ Interview-Problems/1108. Defanging an IP Address.java
@@ -16,3 +16,21 @@ class Solution {
         return res;
     }
 }
+
+
+
+
+
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < address.length(); i++){
+            if (address.charAt(i) == '.'){
+                str.append("[.]");
+            } else {
+                str.append(address.charAt(i));
+            }
+        }
+        return str.toString();
+    }
+}
