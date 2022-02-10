d346b7a47addb01a0b6b71a508a9b8adac1a1463
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-10 15:12:48 +0530

    Update Sliding Window Maximum.java

--- Learning/061_Advanced_Queue and Deque - 2/Sliding Window Maximum.java
+++ Learning/061_Advanced_Queue and Deque - 2/Sliding Window Maximum.java
@@ -126,3 +126,52 @@ Explanation 2:
 
 
 
+
+
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public ArrayList<Integer> slidingMaximum(final List<Integer> A, int B) {
+        
+       
+         int n = A.size();
+        int i;
+        ArrayList < Integer > res = new ArrayList < > ();
+        int window = Math.min(A.size(), B);
+        Deque < Node > deque = new LinkedList < > ();
+        int val;
+        Node ans;
+        for (i = 0; i < window - 1; i++) {
+            val = A.get(i);
+            while (!deque.isEmpty() && deque.peekFirst().val <= val) {
+                deque.pollFirst();
+            }
+            deque.addFirst(new Node(i, val));
+        }
+        for ( i=i;i < n; i++) {
+            val = A.get(i);
+            while (!deque.isEmpty() && (i - deque.peekLast().index >= window)) {
+                deque.pollLast();
+            }
+            while (!deque.isEmpty() && deque.peekFirst().val <= val) {
+                deque.pollFirst();
+            }
+            deque.addFirst(new Node(i, val));
+            ans = deque.peekLast();
+            res.add(ans.val);
+        }
+        return res;
+    }
+
+    class Node {
+        int val;
+        int index;
+        public Node(int index, int val) {
+            this.index = index;
+            this.val = val;
+        }
+    }
+}
+
+
+
+
