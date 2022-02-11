a07ed51be025afa08ec93762ae342ba6db67703f
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-11 18:39:06 +0530

    Create Postorder Traversal.java

---
+++ Learning/062_Advanced_Trees - 1/Postorder Traversal.java
@@ -0,0 +1,86 @@
+Problem Description
+
+Given a binary tree, return the Postorder traversal of its nodes values.
+
+NOTE: Using recursion is not allowed.
+
+
+
+Problem Constraints
+1 <= number of nodes <= 105
+
+
+
+Input Format
+First and only argument is root node of the binary tree, A.
+
+
+
+Output Format
+Return an integer array denoting the Postorder traversal of the given binary tree.
+
+
+
+Example Input
+Input 1:
+
+   1
+    \
+     2
+    /
+   3
+Input 2:
+
+   1
+  / \
+ 6   2
+    /
+   3
+
+
+Example Output
+Output 1:
+
+ [3, 2, 1]
+Output 2:
+
+ [6, 3, 2, 1]
+
+
+Example Explanation
+Explanation 1:
+
+ The Preoder Traversal of the given tree is [3, 2, 1].
+Explanation 2:
+
+ The Preoder Traversal of the given tree is [6, 3, 2, 1].
+   
+   
+   
+   
+   
+   /**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    ArrayList<Integer> arr=new ArrayList<Integer>();
+    public ArrayList<Integer> postorderTraversal(TreeNode A) {
+        if(A==null)return null;
+        else{
+            postorderTraversal(A.left);
+            postorderTraversal(A.right);
+            arr.add(A.val);
+        }
+        return arr;
+    }
+}
