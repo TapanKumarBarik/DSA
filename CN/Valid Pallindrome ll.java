c317209529aeca4f31b9e990a65c066486b62d42
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-15 12:04:46 +0530

    Create Valid Pallindrome ll.java

---
+++ CN/Valid Pallindrome ll.java
@@ -0,0 +1,37 @@
+
+public class Solution {
+ 
+	public static boolean validPalindrome(int n, String s) {
+		// Write your code here
+        
+        
+        int left=0;
+        int right=n-1;
+        
+        while(left<right){
+                  if(s.charAt(left)!=s.charAt(right) ){
+                return isPalinDrome(s,left+1, right) ||isPalinDrome(s,left, right-1);
+            }
+            
+            left++;
+            right--;
+        }
+        return true;
+	}
+    
+    
+    private static boolean isPalinDrome(String S, int left, int right){
+        
+        while(left<right){
+            if(S.charAt(left)!=S.charAt(right) ){
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        return true;
+        
+    }
+
+}
