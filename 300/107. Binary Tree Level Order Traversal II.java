dc7a46ba968b21fffd4640db93a0f61cd5a785c8
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-22 22:19:37 +0530

    Create 107. Binary Tree Level Order Traversal II.java

---
+++ 300/107. Binary Tree Level Order Traversal II.java
@@ -0,0 +1,68 @@
+//https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode>q=new LinkedList();
+        
+        List<List<Integer>>arr=new ArrayList();
+        
+        if(root==null){
+            return arr;
+        }
+        
+        
+        q.add(root);
+        
+        
+        while(!q.isEmpty()){
+            
+            
+            
+            int len=q.size();
+            
+            List<Integer>ar=new ArrayList();
+            
+            for(int i=0;i<len;i++){
+                
+                TreeNode temp=q.poll();
+                ar.add(temp.val);
+                
+                
+                if(temp.left!=null){
+                    q.add(temp.left);
+                }
+                if(temp.right!=null){
+                    q.add(temp.right);
+                }
+                
+               
+            }
+             arr.add(ar);
+        }
+          List<List<Integer>>res=new ArrayList();
+        for(int i=arr.size()-1;i>=0;i--){
+            res.add(arr.get(i));
+        }
+        
+        
+        
+        return res;
+        
+    }
+}
