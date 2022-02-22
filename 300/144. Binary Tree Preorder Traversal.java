ce5b03842a12f567e071a80683a6b5ed8ceb007e
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-22 18:20:39 +0530

    Update 144. Binary Tree Preorder Traversal.java

--- 300/144. Binary Tree Preorder Traversal.java
+++ 300/144. Binary Tree Preorder Traversal.java
@@ -93,3 +93,74 @@ class Solution {
         return arr;
     }
 }
+
+
+//morris
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+       
+        
+        TreeNode pre=null;
+        
+        List<Integer>arr=new ArrayList();
+        
+        while(root!=null){
+            
+            
+            if(root.left==null){
+                
+                arr.add(root.val);
+                root=root.right;
+            }
+            
+            else{
+                
+                pre=root.left;
+                
+                
+                while(pre.right!=null && pre.right !=root){
+                    
+                    pre=pre.right;
+                }
+                
+                if(pre.right==null){
+                    arr.add(root.val);
+                    pre.right=root;
+                    root=root.left;
+                }
+                
+                else{
+                    
+                    pre.right=null;
+                    
+                   // 
+                    
+                    root=root.right;
+                }
+                
+            }
+            
+            
+        }
+        return arr;
+        
+    }
+}
