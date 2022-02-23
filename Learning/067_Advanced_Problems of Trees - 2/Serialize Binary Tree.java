c2b17476f8d02adbd7eb60de534cc029d351ff6c
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-23 14:25:15 +0530

    Create Serialize Binary Tree.java

---
+++ Learning/067_Advanced_Problems of Trees - 2/Serialize Binary Tree.java
@@ -0,0 +1,120 @@
+Problem Description
+
+Given the root node of a Binary Tree denoted by A. You have to Serialize the given Binary Tree in the described format.
+
+Serialize means encode it into a integer array denoting the Level Order Traversal of the given Binary Tree.
+
+NOTE:
+
+In the array, the NULL/None child is denoted by -1.
+For more clarification check the Example Input.
+
+
+Problem Constraints
+1 <= number of nodes <= 105
+
+
+
+Input Format
+Only argument is a A denoting the root node of a Binary Tree.
+
+
+
+Output Format
+Return an integer array denoting the Level Order Traversal of the given Binary Tree.
+
+
+
+Example Input
+Input 1:
+
+           1
+         /   \
+        2     3
+       / \
+      4   5
+Input 2:
+
+            1
+          /   \
+         2     3
+        / \     \
+       4   5     6
+
+
+Example Output
+Output 1:
+
+ [1, 2, 3, 4, 5, -1, -1, -1, -1, -1, -1]
+Output 2:
+
+ [1, 2, 3, 4, 5, -1, 6, -1, -1, -1, -1, -1, -1]
+
+
+Example Explanation
+Explanation 1:
+
+ The Level Order Traversal of the given tree will be [1, 2, 3, 4, 5 , -1, -1, -1, -1, -1, -1].
+ Since 3, 4 and 5 each has both NULL child we had represented that using -1.
+Explanation 2:
+
+ The Level Order Traversal of the given tree will be [1, 2, 3, 4, 5, -1, 6, -1, -1, -1, -1, -1, -1].
+ Since 3 has left child as NULL while 4 and 5 each has both NULL child.
+   
+   
+   
+   
+   
+   
+   
+   /**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> solve(TreeNode A) {
+        
+        ArrayList<Integer>arr=new ArrayList();
+        
+        Queue<TreeNode>q=new LinkedList();
+        
+        q.add(A);
+        arr.add(A.val);
+        
+        while(!q.isEmpty()){
+            
+            
+            TreeNode temp=q.poll();
+            
+            if(temp.left!=null){
+                q.add(temp.left);
+                arr.add(temp.left.val);
+            }
+            else{
+                arr.add(-1);
+            }
+            if(temp.right!=null){
+                q.add(temp.right);
+                arr.add(temp.right.val);
+            }
+            else{
+                arr.add(-1);
+            }
+            
+            
+            
+            
+        }
+        
+        return arr;
+    }
+}
