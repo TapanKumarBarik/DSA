466f01c92a202eb34442d2a14458b60c0e57bd0b
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-26 21:57:48 +0530

    Update 73. Set Matrix Zeroes.java

--- Striver-SDE-sheet/73. Set Matrix Zeroes.java
+++ Striver-SDE-sheet/73. Set Matrix Zeroes.java
@@ -1,1 +1,58 @@
+//https://leetcode.com/problems/set-matrix-zeroes/
 
+
+
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+     
+        
+        
+        //BRUT FORCE
+        
+        int row=matrix.length;
+        
+        int column=matrix[0].length;
+        
+        
+        int col=1;
+        for(int i=0;i<row;i++){
+            
+            
+            if(matrix[i][0]==0){
+                col=0;
+            }
+            for(int j=1;j<column;j++){
+                
+                if(matrix[i][j]==0){
+                    
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                    
+                }
+            }
+        }
+        
+        
+        for(int i=row-1;i>=0;i--){
+            
+            for(int j=column-1;j>=1;j--){
+                
+                if(matrix[i][0]==0 ||  matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+                
+               
+            }
+             if(col==0){
+                    matrix[i][0]=0;
+                }
+        }
+        
+        
+        
+        
+        
+        
+    }
+}
