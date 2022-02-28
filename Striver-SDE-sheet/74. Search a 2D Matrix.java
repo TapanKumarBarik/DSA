dd1b5fdf149b07ee0ee62be40ab0b25275aa71f6
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-28 20:14:59 +0530

    Update 74. Search a 2D Matrix.java

--- Striver-SDE-sheet/74. Search a 2D Matrix.java
+++ Striver-SDE-sheet/74. Search a 2D Matrix.java
@@ -37,3 +37,44 @@ class Solution {
 
 
 //Approach 2 Binary search approach
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+    int row=matrix.length;
+        
+        int column=matrix[0].length;
+        
+        
+        int right=row*column -1;
+        
+        
+        int left=0;
+        
+        
+        while(left<=right){
+            
+            
+            int mid=(left+right)/2;
+            
+            
+            int curr= matrix[mid/column][mid%column];
+            
+            if(curr==target){
+                return true;
+            }
+            
+            if(curr>target){
+                right=mid-1;
+                
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        
+        return false;
+        
+    }
+}
