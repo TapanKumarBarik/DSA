3d1c68eb4bda9df712b67c2d3c17300e30fd79bd
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-02-28 10:51:13 +0530

    Create 287. Find the Duplicate Number.java

---
+++ Striver-SDE-sheet/287. Find the Duplicate Number.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/find-the-duplicate-number/
+
+
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        
+       int fast=nums[0];
+        
+        int slow=nums[0];
+        
+        
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }
+            while(slow!=fast);
+        
+        slow=nums[0];
+        
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        
+        return slow;
+        
+    }
+}
