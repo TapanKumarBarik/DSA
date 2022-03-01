8608c5c30e0b40bd5b4ea6848f6e856cad3590c6
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-03-01 20:41:09 +0530

    Create 62. Unique Paths.java

---
+++ Striver-SDE-sheet/62. Unique Paths.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/unique-paths/
+
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+
+        
+        int N=m+n-2;
+        
+        int R=n-1;
+        
+        double res=1;
+        
+        for(int i=1;i<=R;i++){
+            
+            res=res* (N-R+i)/i;
+        }
+        return (int)res;
+    }
+}
