7ad88652acf63b9789949b0408fdcde1bd83b41c
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-03-07 18:47:49 +0530

    Update B-th Smallest Prime Fraction.java

--- Learning/071_Advanced_Heaps - 1/B-th Smallest Prime Fraction.java
+++ Learning/071_Advanced_Heaps - 1/B-th Smallest Prime Fraction.java
@@ -66,6 +66,34 @@ You only need to implement the given function. Do n
 
 
 
+//
+
+
+
+public class Solution {
+    public int[] solve(int[] arr, int k) {
+        	// max-heap
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            double x = (double)b[0] / b[1];
+            double y = (double)a[0] / a[1];
+            if (x > y)
+				return 1;  // lambda exp. returns b(param of lambda exp), if the value is positive. Hence returned b.
+            return -1;  // lambda exp. returns a(param of lambda exp), if the value is negative. Hence returned a.
+        });
+        
+        for (int i = 0 ; i < arr.length ; i++) {
+            for (int j = i+1 ; j < arr.length ; j++) {
+                pq.offer(new int[]{arr[i], arr[j]});
+				// Avoiding size greater than k
+                while (pq.size() > k) {
+                    pq.poll();
+                }
+            }
+        }
+
+        return pq.poll();
+    }
+}
 
 
 
