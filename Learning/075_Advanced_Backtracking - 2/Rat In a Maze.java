bee09411923f1dc111e5f1e33034769b9d168d8b
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-03-12 21:40:21 +0530

    Create Rat In a Maze.java

---
+++ Learning/075_Advanced_Backtracking - 2/Rat In a Maze.java
@@ -0,0 +1,105 @@
+Problem Description
+
+Given a grid A, a rat is at position (1, 1). He wants to go to the position (n, n) where n is size of the square matrix A.
+
+1 represents a traversable cell and 0 represents a non-traversable cell. Rat can only move right or down.
+
+Return a path that the rat can take.
+
+
+
+Problem Constraints
+1 <= |A| <= 4
+
+
+
+Input Format
+First and only argument is the vector of vectors A.
+
+
+
+Output Format
+Return a vector of vectors that denotes a path that can be taken.
+
+
+
+Example Input
+Input 1:
+
+A = [   [1, 0]
+        [1, 1]
+    ]
+Input 2:
+
+A = [    [1, 1, 1]
+         [1, 0, 1]
+         [0, 0, 1]
+    ]
+
+
+Example Output
+Output 1:
+
+[   [1, 0]
+    [1, 1]
+]
+Output 2:
+
+[    [1, 1, 1]
+     [0, 0, 1]
+     [0, 0, 1]
+]
+
+
+Example Explanation
+Explanation 1:
+
+ Path is shown in output.
+Explanation 2:
+
+ Path is shown in output.
+
+
+   
+   
+   
+   
+   
+   
+   public class Solution {
+    
+    public int[][] solve(int[][] A) {
+        
+        int N=A.length;
+         int sol[][] = new int[N][N];
+        solveMazeUtil(A, 0, 0, sol,N);
+        return sol;
+    }
+
+
+    boolean solveMazeUtil(int maze[][], int x, int y, int sol[][],int N)
+    {
+
+        if (x == N - 1 && y == N - 1 && maze[x][y] == 1) {
+            sol[x][y] = 1;
+            return true;
+        }
+
+        if (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 1) {
+              if (sol[x][y] == 1)return false;
+                  
+            sol[x][y] = 1;
+            
+            if (solveMazeUtil(maze, x + 1, y, sol,N)) return true;
+            if (solveMazeUtil(maze, x, y + 1, sol,N)) return true;
+            if (solveMazeUtil(maze, x - 1, y, sol,N)) return true;
+            if (solveMazeUtil(maze, x, y - 1, sol,N)) return true;
+
+            sol[x][y] = 0;
+            
+            return false;
+        }
+ 
+        return false;
+    }
+}
