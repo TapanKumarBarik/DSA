b72b41ced7c5d429f7dff66a6b652ef992838aca
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-03-14 08:02:28 +0530

    Create Leaders in an array.java

---
+++ gfg/Leaders in an array.java
@@ -0,0 +1,90 @@
+Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader. 
+
+ 
+
+Example 1:
+
+Input:
+n = 6
+A[] = {16,17,4,3,5,2}
+Output: 17 5 2
+Explanation: The first leader is 17 
+as it is greater than all the elements
+to its right.  Similarly, the next 
+leader is 5. The right most element 
+is always a leader so it is also 
+included.
+ 
+
+Example 2:
+
+Input:
+n = 5
+A[] = {1,2,3,4,0}
+Output: 4 0
+ 
+
+Your Task:
+You don't need to read input or print anything. The task is to complete the function leader() which takes array A and n as input parameters and returns an array of leaders in order of their appearance.
+
+ 
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+ 
+
+Constraints:
+1 <= n <= 107
+0 <= Ai <= 107
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int A[], int n){
+        // Your code here
+        
+      ArrayList<Integer> result = new ArrayList<>();
+        int max_right = A[A.length-1];
+        result.add(max_right);
+
+        for(int j=A.length-2; j>=0; j--) {
+            if (A[j] >= max_right) {
+                result.add(A[j]);
+                max_right = A[j];
+            }
+        }
+        
+        Collections.reverse(result);
+        return result;
+    
+//       int maxEle = arr[n-1];
+        
+//         ArrayList<Integer> res = new ArrayList<>();
+        
+//         //We start traversing the array from last element.
+//         for(int i=n-1; i>=0; i--) {
+            
+//             //Comparing the current element with the maximum element stored. 
+//             //If current element is greater than max, we add the element.
+// 		    if(arr[i] >= maxEle){
+// 		        //Updating the maximum element.
+// 		        maxEle = arr[i];
+// 		        //Storing the current element in arraylist for leaders.
+// 		        res.add(maxEle);
+// 		    }
+// 		}
+		
+// 		//Reversing the arraylist.
+// 		Collections.reverse(res);
+// 		//returning the arraylist.
+//         return res;
+    }
+}
