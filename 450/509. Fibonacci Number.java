2312a9347a3f5330f48b791db03e8c0a813e35b6
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-03-19 20:44:48 +0530

    Update 509. Fibonacci Number.java

--- 450/509. Fibonacci Number.java
+++ 450/509. Fibonacci Number.java
@@ -8,3 +8,27 @@ class Solution {
          return fib(n-1)+fib(n-2);
     }
 }
+
+
+
+class Solution {
+    public int fib(int n) {
+        int fib1=0;
+        int fib2=1;
+        
+        
+        if(n<2)return n;
+        
+        int fib3=0;
+        
+        for(int i=2;i<=n;i++){
+            
+            fib3=fib1+fib2;
+            
+            fib1=fib2;
+            fib2=fib3;
+            
+        }
+        return fib3;
+    }
+}
