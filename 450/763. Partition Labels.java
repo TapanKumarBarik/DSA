83322e19efd0f222067c0926fd29d42a86a7c935
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-03-19 20:40:28 +0530

    Update 763. Partition Labels.java

--- 450/763. Partition Labels.java
+++ 450/763. Partition Labels.java
@@ -1,6 +1,6 @@
 https://leetcode.com/problems/partition-labels/
 
-
+//Array
 class Solution {
     public List<Integer> partitionLabels(String S) {
         int[] last = new int[26];
@@ -20,5 +20,47 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+
+
+//map
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        
+     List<Integer> list = new ArrayList<>();
+
+       HashMap<Character,Integer>map=new HashMap();
+        
+        int n=s.length();
+        
+        for(int i=0;i<n;i++){
+            map.put(s.charAt(i),i );
+        }
+        
+        
+        int prev=-1;
+        
+        int max=0;
+        
+        
+        for(int i=0;i<n;i++){
+            char curr=s.charAt(i);
+            
+            max=Math.max(map.get(s.charAt(i)),max);
+            
+        
+            
+            if(max==i ){
+                list.add(max-prev);
+                
+                prev=max;
+                
+            }
+        }
+        
+        return list;
     }
 }
