6bd458311d7f9afcfd0791bba641216cfb97d125
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-03-19 15:27:41 +0530

    Update 747. Largest Number At Least Twice of Others.java

--- 450/747. Largest Number At Least Twice of Others.java
+++ 450/747. Largest Number At Least Twice of Others.java
@@ -22,3 +22,31 @@ class Solution {
         return -1;
     }
 }
+
+
+
+class Solution {
+    public int dominantIndex(int[] nums) {
+        
+        
+        int n=nums.length;
+        if(n==1)return 0;
+        
+        
+        int max=Integer.MIN_VALUE;
+        int maxindex=-1;;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]>max){
+                max=nums[i];
+                maxindex=i;
+            }
+        }
+        Arrays.sort(nums);
+        
+        if(nums[n-1]>=2*nums[n-2]){
+            return maxindex;
+        }
+        return -1;
+    }
+}
