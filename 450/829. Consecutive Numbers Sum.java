b25df93345f095dc358155d8ecfe2bef547928a7
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-03-20 12:03:03 +0530

    Create 829. Consecutive Numbers Sum.java

---
+++ 450/829. Consecutive Numbers Sum.java
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/consecutive-numbers-sum/
+
+
+class Solution {
+    public int consecutiveNumbersSum(int N) {
+        
+        
+   int res=0;
+        int i=1;
+        
+        while(N>0){
+            N-=i;
+            
+            if(N%i==0){
+                res++;
+            }
+            i++;
+        }
+        
+        return res;
+    }
+}
