4033e23f305378040e224ce80d4716abd264fec0
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-03-28 17:04:54 +0530

    Create Unbounded Knapsack.java

---
+++ Learning/079_Advanced_Dynamic Programming - 4/Unbounded Knapsack.java
@@ -0,0 +1,93 @@
+Problem Description
+
+Given a knapsack weight A and a set of items with certain value B[i] and weight C[i], we need to calculate maximum amount that could fit in this quantity.
+
+This is different from classical Knapsack problem, here we are allowed to use unlimited number of instances of an item.
+
+
+
+Problem Constraints
+1 <= A <= 1000
+
+1 <= |B| <= 1000
+
+1 <= B[i] <= 1000
+
+1 <= C[i] <= 1000
+
+
+
+Input Format
+First argument is the Weight of knapsack A
+
+Second argument is the vector of values B
+
+Third argument is the vector of weights C
+
+
+
+Output Format
+Return the maximum value that fills the knapsack completely
+
+
+
+Example Input
+Input 1:
+
+A = 10
+B = [5]
+C = [10]
+Input 2:
+
+A = 10
+B = [6, 7]
+C = [5, 5]
+
+
+Example Output
+Output 1:
+
+ 5
+Output 2:
+
+14
+
+
+Example Explanation
+Explanation 1:
+
+Only valid possibility is to take the given item.
+Explanation 2:
+
+Take the second item twice.
+
+
+
+×
+
+
+
+
+
+
+public class Solution {
+    public int solve( int W,int[] val, int[] wt) {
+        int n=val.length;
+   // dp[i] is going to store maximum value
+        // with knapsack capacity i.
+        int dp[] = new int[W + 1];
+         
+        // Fill dp[] using above recursive formula
+        for(int i = 0; i <= W; i++){
+            for(int j = 0; j < n; j++){
+                if(wt[j] <= i){
+                    dp[i] = Math.max(dp[i], dp[i - wt[j]] +
+                                val[j]);
+                }
+            }
+        }
+        return dp[W];
+
+    
+    }
+}
