8e00a84b24dea55036b29301b1bbf0737704e54d
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-04-22 09:57:58 +0530

    Create Path in Directed Graph.java

---
+++ Learning/082 - Advanced DSA : Graphs - 1/Path in Directed Graph.java
@@ -0,0 +1,112 @@
+Problem Description
+
+Given an directed graph having A nodes labelled from 1 to A containing M edges given by matrix B of size M x 2such that there is a edge directed from node
+
+B[i][0] to node B[i][1].
+
+Find whether a path exists from node 1 to node A.
+
+Return 1 if path exists else return 0.
+
+NOTE:
+
+There are no self-loops in the graph.
+There are no multiple edges between two nodes.
+The graph may or may not be connected.
+Nodes are numbered from 1 to A.
+Your solution will run on multiple test cases. If you are using global variables make sure to clear them.
+
+
+Problem Constraints
+2 <= A <= 105
+
+1 <= M <= min(200000,A*(A-1))
+
+1 <= B[i][0], B[i][1] <= A
+
+
+
+Input Format
+The first argument given is an integer A representing the number of nodes in the graph.
+
+The second argument given a matrix B of size M x 2 which represents the M edges such that there is a edge directed from node B[i][0] to node B[i][1].
+
+
+
+Output Format
+Return 1 if path exists between node 1 to node A else return 0.
+
+
+
+Example Input
+Input 1:
+
+ A = 5
+ B = [  [1, 2] 
+        [4, 1] 
+        [2, 4] 
+        [3, 4] 
+        [5, 2] 
+        [1, 3] ]
+Input 2:
+
+ A = 5
+ B = [  [1, 2]
+        [2, 3] 
+        [3, 4] 
+        [4, 5] ]
+
+
+Example Output
+Output 1:
+
+ 0
+Output 2:
+
+ 1
+
+
+Example Explanation
+Explanation 1:
+
+ The given doens't contain any path from node 1 to node 5 so we will return 0.
+Explanation 2:
+
+ Path from node1 to node 5 is ( 1 -> 2 -> 3 -> 4 -> 5 ) so we will return 1.
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   public class Solution {
+    public int solve(int A, int[][] B) {
+        
+        ArrayList<ArrayList<Integer>>adj=new ArrayList();
+        for(int i=0;i<=100009;i++){
+            adj.add(new ArrayList());
+        }
+        //
+        for(int i=0;i<B.length;i++){
+            ArrayList<Integer>curr=adj.get(B[i][0]);
+            curr.add(B[i][1]);
+            adj.set(B[i][0], curr ) ;
+        }
+        Queue<Integer>q=new LinkedList();
+        q.offer(1);
+        while(!q.isEmpty()){
+            
+            int curr=q.poll();
+            if(curr==A)return 1;
+            ArrayList<Integer>temp=adj.get(curr);
+            for(int i=0;i<temp.size();i++ ){
+                q.offer(temp.get(i) );
+            }
+        }
+       
+        return 0;
+    }
+}
